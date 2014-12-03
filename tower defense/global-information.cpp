a26593399923b128a53909e8c991d4de20f1af33
makowiecki <makowiec.review@outlook.com>
2014-12-03 17:14:15 +0100

    Added file with some global information.

---
+++ tower defense/global-information.cpp
@@ -0,0 +1,12 @@
+#include "global-information.h"
+
+namespace gi
+{
+	unsigned int WINDOW_WIDTH=FIELDS_IN_ROW*FIELD_WIDTH;
+	unsigned int WINDOW_HEIGHT=FIELDS_IN_COLUMN*FIELD_HEIGHT;
+
+	unsigned int FIELDS_IN_ROW=27;
+	unsigned int FIELDS_IN_COLUMN=15;
+	unsigned int FIELD_WIDTH=50;
+	unsigned int FIELD_HEIGHT=50;
+}
---
+++ tower defense/global-information.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace gi
+{
+	extern unsigned int WINDOW_WIDTH;
+	extern unsigned int WINDOW_HEIGHT;
+
+	extern unsigned int FIELDS_IN_ROW;
+	extern unsigned int FIELDS_IN_COLUMN;
+	extern unsigned int FIELD_WIDTH;
+	extern unsigned int FIELD_HEIGHT;
+
+}
