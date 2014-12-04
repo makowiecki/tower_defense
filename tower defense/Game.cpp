87cb8ae32015ae3b8adf41fe16a84409fd404164
makowiecki <makowiec.review@outlook.com>
2014-12-04 16:30:01 +0100

    Updated creation Board with new Fields.

--- tower defense/Game.cpp
+++ tower defense/Game.cpp
@@ -2,6 +2,9 @@
 
 #include "global-information.h"
 #include "FieldWall.h"
+#include "FieldEmpty.h"
+#include "FieldEnter.h"
+#include "FieldExit.h"
 
 #include <iostream>
 using namespace std;
@@ -19,28 +22,26 @@ Game::Game()
 	{
 		for(unsigned int j=0; j < gi::FIELDS_IN_COLUMN; ++j)
 		{
-			mBoard[i][j]=new FieldWall(i*gi::FIELD_WIDTH, j*gi::FIELD_HEIGHT);
-
-			//			if(i == 0 && j == gi::FIELDS_IN_COLUMN / 2)
-			//			{
-			//				mFieldsList.emplace_back(Field(Field::ENTER, i*gi::FIELD_WIDTH, j*gi::FIELD_HEIGHT));
-			//			}
-			//			else if(i == gi::FIELDS_IN_ROW - 1 && j == gi::FIELDS_IN_COLUMN / 2)
-			//			{
-			//				mFieldsList.emplace_back(Field(Field::EXIT, i*gi::FIELD_WIDTH, j*gi::FIELD_HEIGHT));
-			//			}
-			//			else if(i == 0 || i == gi::FIELDS_IN_ROW - 1)
-			//			{
-			//				mFieldsList.emplace_back(Field(Field::WALL, i*gi::FIELD_WIDTH, j*gi::FIELD_HEIGHT));
-			//			}
-			//			else if(j == 0 || j == gi::FIELDS_IN_COLUMN - 1)
-			//			{
-			//				mFieldsList.emplace_back(Field(Field::WALL, i*gi::FIELD_WIDTH, j*gi::FIELD_HEIGHT));
-			//			}
-			//			else
-			//			{
-			//				mFieldsList.emplace_back(Field(Field::EMPTY, i*gi::FIELD_WIDTH, j*gi::FIELD_HEIGHT));
-			//			}
+			if(i == 0 && j == gi::FIELDS_IN_COLUMN / 2)
+			{
+				mBoard[i][j]=new FieldEnter(i*gi::FIELD_WIDTH, j*gi::FIELD_HEIGHT);
+			}
+			else if(i == gi::FIELDS_IN_ROW - 1 && j == gi::FIELDS_IN_COLUMN / 2)
+			{
+				mBoard[i][j]=new FieldExit(i*gi::FIELD_WIDTH, j*gi::FIELD_HEIGHT);
+			}
+			else if(i == 0 || i == gi::FIELDS_IN_ROW - 1)
+			{
+				mBoard[i][j]=new FieldWall(i*gi::FIELD_WIDTH, j*gi::FIELD_HEIGHT);
+			}
+			else if(j == 0 || j == gi::FIELDS_IN_COLUMN - 1)
+			{
+				mBoard[i][j]=new FieldWall(i*gi::FIELD_WIDTH, j*gi::FIELD_HEIGHT);
+			}
+			else
+			{
+				mBoard[i][j]=new FieldEmpty(i*gi::FIELD_WIDTH, j*gi::FIELD_HEIGHT);
+			}
 		}
 	}
 }
