ab7cba3f64c34cae720cf1d9dc42fc05c6f5a964
makowiecki <makowiec.review@outlook.com>
2015-01-07 22:23:20 +0100

    Way added to Enemy. Testing code in update function. Some names changed.

--- tower defense/Enemy.cpp
+++ tower defense/Enemy.cpp
@@ -1,16 +1,17 @@
 #include "Enemy.h"
 
 #include "ResourceManager.h"
+#include "FieldManager.h"
 #include "global-information.h"
 
 #include <iostream>
 using namespace std;
 
-Enemy::Enemy(int x, int y):mAggregateTime(0.0f)
+Enemy::Enemy(int pX, int pY, const Board& board):mWay(board), mAggregateTime(0.0f)
 {
 	mEnemySprite.setTexture(ResourceManager::getTexture(ResourceManager::ENEMY_1));
 	mEnemySprite.setOrigin(mEnemySprite.getGlobalBounds().height / 2, mEnemySprite.getGlobalBounds().width / 2);
-	mEnemySprite.setPosition(static_cast<float>(x), static_cast<float>(y));
+	mEnemySprite.setPosition(static_cast<float>(pX), static_cast<float>(pY));
 }
 
 
@@ -23,13 +24,18 @@ void Enemy::draw(sf::RenderTarget& target, sf::RenderStates states)const
 	target.draw(mEnemySprite);
 }
 
-void Enemy::update(const sf::RenderWindow& window, float dt)
+void Enemy::update(const sf::RenderWindow& window, float dt, const Board& board)
 {
-	mAggregateTime+=dt;
-
-	if(mAggregateTime > 1.0f)
+	if(!mWay.canFind(board, mEnemySprite.getPosition()))
 	{
-		mEnemySprite.move(static_cast<float>(gi::FIELD_WIDTH), 0);
-		mAggregateTime=0.0f;
+		FieldManager::getInstance().discardChange();
 	}
+
+	//mAggregateTime+=dt;
+
+	//if(mAggregateTime > 1.0f)
+	//{
+	//	mEnemySprite.move(static_cast<float>(gi::FIELD_WIDTH), 0);
+	//	mAggregateTime=0.0f;
+	//}
 }
--- tower defense/Enemy.h
+++ tower defense/Enemy.h
@@ -1,21 +1,24 @@
 #pragma once
 
 #include <SFML\Graphics.hpp>
+#include "Way.h"
+#include "Board.h"
 
 class Enemy:	public sf::Drawable
 {
 protected:
 
 	sf::Sprite mEnemySprite;
+	Way mWay;
 	//int mID;
 	float mAggregateTime;
 
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states)const;
 
 public:
-	Enemy(int x, int y);
+	Enemy(int pX, int pY, const Board& board);
 	virtual ~Enemy();
 
-	virtual void update(const sf::RenderWindow& window, float dt);
+	virtual void update(const sf::RenderWindow& window, float dt, const Board& board);
 };
 
