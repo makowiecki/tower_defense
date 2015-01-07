82bd521fac6be064fc82fed24f24d4df221ca193
makowiecki <makowiec.review@outlook.com>
2015-01-07 23:22:28 +0100

    Added Way.

--- tower defense/Way.h
+++ tower defense/Way.h
@@ -1,21 +1,35 @@
 #ifndef WAY_H
 #define WAY_H
 
+#include <SFML\Graphics.hpp>
 #include <vector>
-#include "Field.h"
-
-
-bool findWay(const std::vector<std::vector<Field*>>& board, unsigned int destinationX, unsigned int destinationY);
-
-
+#include <stack>
+#include "Board.h"
 
 class Way
 {
+private:
+
+	std::vector<std::vector<int>> mWorkingBoard;
+	std::stack<sf::Vector2i> mStepsList;
+	sf::Vector2i mExitPosition;
+
+	void makeCopy(const Board& board);
+	void fillStepsList(int k);
+
 public:
-	Way();
+	Way(const Board& board);
 	~Way();
 
-	
+	////
+	///
+	///	\brief Check whether can find way if player will block chosen Field on board.
+	///
+	///	\return True if can find way, false if cannot find way while chosen Filed will be blocked.
+	////
+	bool canFind(const Board& board, const sf::Vector2f& pixelsStartPosition);
+
+	sf::Vector2i getNextStep();
 };
 
 #endif
