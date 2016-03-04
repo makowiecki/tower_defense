c84ea972c9f4379bec0f8d8b54577b2dd8415e08
makowiecki <makowiec.review@outlook.com>
2016-03-04 16:39:22 +0100

    Improved Monster bahaviour.

--- tower defense/Monster.cpp
+++ tower defense/Monster.cpp
@@ -4,13 +4,27 @@
 #include "FieldManager.h"
 #include "global-information.h"
 
+
+#include <iostream>
+using namespace std;
+#include <conio.h>
+
 Monster::Monster(int pX, int pY, std::stack<sf::Vector2i>& stepsList)
 	:mStepsList(stepsList),
-	mAggregateTime(0.0f)
+	mAggregateTime(0.0f),
+	mDirection(Direction::RIGHT),
+	mSpeed(50.0f)
 {
 	mMonsterSprite.setTexture(ResourceManager::getTexture(ResourceManager::ENEMY_1));
 	mMonsterSprite.setOrigin(mMonsterSprite.getGlobalBounds().height / 2, mMonsterSprite.getGlobalBounds().width / 2);
 	mMonsterSprite.setPosition(static_cast<float>(pX), static_cast<float>(pY));
+	
+	mNextStep=mStepsList.top();
+	mStepsList.pop();
+	mPreviousStep.x=pX / gi::FIELD_WIDTH;
+	mPreviousStep.y=pY / gi::FIELD_HEIGHT;
+	
+	setDirection();
 }
 
 Monster::~Monster()
@@ -20,6 +34,57 @@ Monster::~Monster()
 void Monster::draw(sf::RenderTarget& target, sf::RenderStates states)const
 {
 	target.draw(mMonsterSprite);
+}
+
+void Monster::setDirection()
+{
+	//sf::Vector2i nextPosition;
+	//sf::Vector2i monsterPosition=static_cast<sf::Vector2i>(mMonsterSprite.getPosition());
+
+	//nextPosition.x=(nextStep.x*gi::FIELD_WIDTH + 25);
+	//nextPosition.y=(nextStep.y*gi::FIELD_HEIGHT + 25);
+
+
+	cout << "ns.x= " << mNextStep.x << " | ps.x= " << mPreviousStep.x << " || ns.y= " << mNextStep.y << " | ps.y= " << mPreviousStep.y << endl;
+	//_getch();
+
+	if(mNextStep.x > mPreviousStep.x)
+	{
+		mDirection=Direction::RIGHT;
+	}
+	else if(mNextStep.x < mPreviousStep.x)
+	{
+		mDirection=Direction::LEFT;
+	}
+	else if(mNextStep.y > mPreviousStep.y)
+	{
+		mDirection=Direction::DOWN;
+	}
+	else if(mNextStep.y < mPreviousStep.y)
+	{
+		mDirection=Direction::UP;
+	}
+}
+
+void Monster::moveToDirection(float dt)
+{
+	switch(mDirection)
+	{
+		case Direction::LEFT:
+			mMonsterSprite.move(-mSpeed*dt, 0);
+			break;
+		case Direction::RIGHT:
+			mMonsterSprite.move(mSpeed*dt, 0);
+			break;
+		case Direction::UP:
+			mMonsterSprite.move(0, -mSpeed*dt);
+			break;
+		case Direction::DOWN:
+			mMonsterSprite.move(0, mSpeed*dt);
+			break;
+		default:
+			break;
+	}
 }
 
 bool Monster::isExitReached()const
@@ -51,24 +116,20 @@ void Monster::update(const sf::RenderWindow& window, float dt)
 	
 	if(mAggregateTime > 1.0f)
 	{
-		sf::Vector2i nextStep;
-
 		if(mStepsList.size() != 0)
 		{
-			nextStep=mStepsList.top();
+			mPreviousStep=mNextStep;
+
+			mNextStep=mStepsList.top();
 			mStepsList.pop();
 		}
 		else
 		{
 			return;
 		}
-		sf::Vector2f nextPosition;
 
-		nextPosition.x=static_cast<float>(nextStep.x*gi::FIELD_WIDTH + 25);
-		nextPosition.y=static_cast<float>(nextStep.y*gi::FIELD_HEIGHT + 25);
-
-		mMonsterSprite.setPosition(nextPosition);
-
+		setDirection();
 		mAggregateTime=0.0f;
 	}
+	moveToDirection(dt);
 }
--- tower defense/Monster.h
+++ tower defense/Monster.h
@@ -7,11 +7,19 @@ class Monster:	public sf::Drawable
 {
 protected:
 
+	enum Direction { LEFT, RIGHT, UP, DOWN };
+
 	sf::Sprite mMonsterSprite;
 	std::stack<sf::Vector2i> mStepsList;
 	float mAggregateTime;
+	Direction mDirection;
+	float mSpeed;
+	sf::Vector2i mNextStep;
+	sf::Vector2i mPreviousStep;
 
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states)const;
+	void setDirection();
+	void moveToDirection(float dt);
 
 public:
 	Monster(int pX, int pY, std::stack<sf::Vector2i>& stepsList);
