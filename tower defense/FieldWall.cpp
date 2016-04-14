d99254c3e2cd5c4a17fa882485a8ee61a7fbe0a6
makowiecki <makowiec.review@outlook.com>
2016-04-14 15:45:50 +0200

    Added wall images types.

--- tower defense/FieldWall.cpp
+++ tower defense/FieldWall.cpp
@@ -1,6 +1,8 @@
 #include "FieldWall.h"
 #include "ResourceManager.h"
 
+#include "global-information.h"
+
 FieldWall::FieldWall(int x, int y):Field(x, y)
 {
 	init();
@@ -18,7 +20,33 @@ FieldWall::~FieldWall()
 void FieldWall::init()
 {
 	mType=FieldType::FIELD_WALL;
-	mFieldSprite.setTexture(ResourceManager::getTexture(ResourceManager::FIELD_WALL));
+
+	if(isCorner())
+	{
+		static int howManyCorners = 0;
+
+		mFieldSprite.setTexture(ResourceManager::getTexture(ResourceManager::FIELD_WALL_CORNER));
+		
+		sf::IntRect subTexture(0, 0, gi::FIELD_WIDTH, gi::FIELD_HEIGHT); // always 4 corners
+
+		subTexture.top = gi::FIELD_HEIGHT * howManyCorners++;
+
+		mFieldSprite.setTextureRect(subTexture);
+	}
+	else if(getPosition().x == 0 || getPosition().x == gi::WINDOW_WIDTH - gi::FIELD_WIDTH) //vertical walls
+	{
+		mFieldSprite.setTexture(ResourceManager::getTexture(ResourceManager::FIELD_WALL_VERTICAL));
+		mFieldSprite.setTextureRect(sf::IntRect(0, getPosition().x == 0 ? 0 : gi::FIELD_HEIGHT, gi::FIELD_WIDTH, gi::FIELD_HEIGHT));
+	}
+	else if(getPosition().y == 0 || getPosition().y == gi::WINDOW_HEIGHT - gi::FIELD_HEIGHT) //horizontal walls
+	{
+		mFieldSprite.setTexture(ResourceManager::getTexture(ResourceManager::FIELD_WALL_HORIZONTAL));
+		mFieldSprite.setTextureRect(sf::IntRect(0, getPosition().y == 0 ? 0 : gi::FIELD_HEIGHT, gi::FIELD_WIDTH, gi::FIELD_HEIGHT));
+	}
+	else //wall in the middle
+	{
+		mFieldSprite.setTexture(ResourceManager::getTexture(ResourceManager::FIELD_WALL_MIDDLE));
+	}
 }
 
 void FieldWall::update(const sf::RenderWindow& window, float dt)
